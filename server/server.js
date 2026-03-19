034c08db061821eb54839d64f7edfa1aa6693e98
Lakshya Saxena <saxena.lakshya0106@gmail.com>
2026-03-19 23:59:28 +0530

    Basic structure for main server

--- server/server.js
+++ server/server.js
@@ -1,17 +1,37 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { client as supabase } from "./client.js";
 
+// Defining the PORT and Express.js
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(express.json());
+// Middleware for processing the data
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 
-app.get("/", (req, res) => {
+// Health checking route for the services
+app.get("/", async (req, res) => {
+    const { data, error } = await supabase.storage.getBucket("images");
+    let dbStatus = (error) ? "DB is currently down!" : "DB is running healthy!";
+
     res.json({
-        "message": "Server is running",
+        "serverMessage": "Server is running",
+        "dbMessage": dbStatus,
         "status": 200,
     });
 });
 
+// Uploading and sending the image to Supabase Storage
+app.post("/upload", (req, res) => {
+    console.log(req.body);
+    res.json({
+        "message": "Image received successfully!",
+        "status": 200,
+    });
+});
+
+// Making the API alive
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
