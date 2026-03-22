48d4d11d8e8004e5e194c5184609e1fdebf7a280
Lakshya Saxena <saxena.lakshya0106@gmail.com>
2026-03-22 22:18:56 +0530

    added image upload to supabase

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
+import multer from "multer";
 import { client as supabase } from "./supabase/client";
 import { ClerkData } from "./types/types";
 
 // Defining the Express.js
 const app = express();
+const upload = multer();
 
 // Middleware for processing data
 app.use(bodyParser.json({ limit: '10mb' }));
@@ -12,66 +14,114 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 
 // Health checking route for the services
 app.get("/", async (req: Request, res: Response) => {
-    const { data, error } = await supabase.storage.getBucket("images");
-    let dbStatus: string = (error) ? "DB is currently down!" : "DB is running healthy!";
+    try {
+        const { data, error } = await supabase.storage.getBucket("images");
+        let dbStatus: string = (error) ? "DB is currently down!" : "DB is running healthy!";
 
-    res.json({
-        "serverMessage": "Server is running",
-        "dbMessage": dbStatus,
-    }).sendStatus(200);
+        return res.json({
+            "serverMessage": "Server is running",
+            "dbMessage": dbStatus,
+        }).sendStatus(200);
+    } catch (error) {
+        console.log("Error checking Main Server");
+        return res.sendStatus(500);
+    }
 });
 
 // Webhook user data sending to Supabase
 app.post("/clerk", async (req: Request, res: Response) => {
-    const clerkData: ClerkData = req.body;
+    try {
+        const clerkData: ClerkData = req.body;
 
-    // User created
-    if (clerkData.eventType === "user.created") {
-        const { data, error } = await supabase.from("users").insert({
-            id: clerkData.id,
-            first_name: clerkData.first_name,
-            last_name: clerkData.last_name,
-            email: clerkData.email,
-            image_url: clerkData.image_url,
+        // User created
+        if (clerkData.eventType === "user.created") {
+            const { data, error } = await supabase.from("users").insert({
+                id: clerkData.id,
+                first_name: clerkData.first_name,
+                last_name: clerkData.last_name,
+                email: clerkData.email,
+                image_url: clerkData.image_url,
+            });
+
+            if (error) {
+                console.log("Error creating user: ", error);
+                return res.sendStatus(500);
+            } else {
+                console.log("User created successfully.");
+                return res.sendStatus(201);
+            }
+        }
+
+        // User updated
+        if (clerkData.eventType === "user.updated") {
+            const { data, error } = await supabase.from("users").update({
+                first_name: clerkData.first_name,
+                last_name: clerkData.last_name,
+                image_url: clerkData.image_url,
+            }).eq("id", clerkData.id);
+
+            if (error) {
+                console.log("Error updating user: ", error);
+                return res.sendStatus(500);
+            } else {
+                console.log("User updated successfully.");
+                return res.sendStatus(200);
+            }
+        }
+
+        // User deleted
+        if (clerkData.eventType === "user.deleted") {
+            const { data, error } = await supabase.from("users").delete().eq("id", clerkData.id);
+
+            if (error) {
+                console.log("Error deleting user: ", error);
+                return res.sendStatus(500);
+            } else {
+                console.log("User deleted succeessfully.");
+                return res.sendStatus(200);
+            }
+        }
+    } catch (error) {
+        console.log("Error storing the user data using Webhook");
+        return res.sendStatus(500);
+    }
+});
+
+// Uploading the image the Supabase storage
+app.post("/upload", upload.single("file"), async (req: Request, res: Response) => {
+    try {
+        const file = req.file;
+
+        // File not found
+        if (!file) {
+            console.log("File not found!");
+            return res.sendStatus(500);
+        }
+
+        // Uploading the image to Supabase
+        const fileName = `${Date.now()}-${file!.originalname}`;
+        const { data, error } = await supabase.storage
+            .from("images")
+            .upload(fileName, file!.buffer!, {
+                contentType: file!.mimetype,
+            })
+
+        if (error) {
+            console.log("File uploading failed!", error);
+            return res.sendStatus(500);
+        }
+
+        // Getting the public URL of the image
+        const publicUrl: string = supabase.storage
+            .from("images")
+            .getPublicUrl(data.path).data.publicUrl;
+
+        return res.json({
+            url: publicUrl,
         });
-
-        if (error) {
-            console.log("Error creating user: ", error);
-            res.sendStatus(500);
-        } else {
-            console.log("User created successfully.");
-            res.sendStatus(201);
-        }
-    }
-
-    // User updated
-    if (clerkData.eventType === "user.updated") {
-        const { data, error } = await supabase.from("users").update({
-            first_name: clerkData.first_name,
-            last_name: clerkData.last_name,
-            image_url: clerkData.image_url,
-        }).eq("id", clerkData.id);
-
-        if (error) {
-            console.log("Error updating user: ", error);
-            res.sendStatus(500);
-        } else {
-            console.log("User updated successfully.");
-            res.sendStatus(200);
-        }
-    }
-
-    // User deleted
-    if (clerkData.eventType === "user.deleted") {
-        const { data, error } = await supabase.from("users").delete().eq("id", clerkData.id);
-
-        if (error) {
-            console.log("Error deleting user: ", error);
-            res.sendStatus(500);
-        } else {
-            console.log("User deleted succeessfully.");
-            res.sendStatus(200);
-        }
+    } catch (error) {
+        console.log("Error uploading the image to Storage");
+        return res.sendStatus(500);
     }
 });
 
--- server/src/supabase/client.ts
+++ server/src/supabase/client.ts
@@ -3,4 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const client = createClient(process.env.SUPABASE_PROJECT_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!);
+export const client = createClient(
+    process.env.SUPABASE_PROJECT_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
+);
